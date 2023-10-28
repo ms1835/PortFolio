69427b8c0ed1bab7a1b4cdb94a6cdedac5d27cb7
Mayank Singh <mayank.singh@digite.com>
2023-10-28 13:18:20 +0530

    Responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-  const scrollToSection = (id) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const scrollToSection = id => {
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth' });
   }
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <nav className="bg-gray-800 p-4 fixed w-full z-10">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          <div className="text-white font-bold text-xl">Your Name</div>
-          <ul className="flex space-x-4">
-            <li className="cursor-pointer"><button onClick={() => scrollToSection('home')} className="text-white">Home</button></li>
-            <li className="cursor-pointer"><button onClick={() => scrollToSection('experience')} className="text-white">Experience</button></li>
-            <li className="cursor-pointer"><button onClick={() => scrollToSection('projects')} className="text-white">Projects</button></li>
-            <li className="cursor-pointer"><button onClick={() => scrollToSection('tech-stack')} className="text-white">Tech Stack</button></li>
-            <li className="cursor-pointer"><button onClick={() => scrollToSection('contact')} className="text-white">Contact</button></li>
-          </ul>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <h1 className="text-white text-2xl font-bold">Your Name</h1>
+        </div>
+        <div className="hidden md:flex space-x-4">
+          <button className="text-white" onClick={() => scrollToSection('home')}>Home</button>
+          <button className="text-white" onClick={() => scrollToSection('experience')}>Projects</button>
+          <button className="text-white" onClick={() => scrollToSection('projects')}>Experience</button>
+          <button className="text-white" onClick={() => scrollToSection('tech-stack')}>Tech Stack</button>
+          <button className="text-white" onClick={() => scrollToSection('contact')}>Contact</button>
+        </div>
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
         </div>
       </div>
+      {showMenu && (
+        <div className="md:hidden py-2 mt-4">
+          <button className="w-full block text-white px-4 py-2 hover:bg-white hover:text-black rounded-md" onClick={() => scrollToSection('home')}>Home</button>
+          <button className="w-full block text-white px-4 py-2 hover:bg-white hover:text-black rounded-md" onClick={() => scrollToSection('projects')}>Projects</button>
+          <button className="w-full block text-white px-4 py-2 hover:bg-white hover:text-black rounded-md" onClick={() => scrollToSection('experience')}>Experience</button>
+          <button className="w-full block text-white px-4 py-2 hover:bg-white hover:text-black rounded-md" onClick={() => scrollToSection('tech-stack')}>Tech Stack</button>
+          <button className="w-full block text-white px-4 py-2 hover:bg-white hover:text-black rounded-md" onClick={() => scrollToSection('contact')}>Contact</button>
+        </div>
+      )}
     </nav>
   );
-}
+};
 
 export default Navbar;
